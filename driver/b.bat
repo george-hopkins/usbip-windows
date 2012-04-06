60f2b5ab4f5b92ca50354f5b6b3fc0fab4807055
cpeacock <cpeacock@users.sourceforge.net>
2012-04-06 12:22:11 +0000

    Added Windows 8 x86 and x64 Editions to the Windows Version List of Inf2Cat ready for driver signing.

--- driver/b.bat
+++ driver/b.bat
@@ -27,7 +27,7 @@ rem sign files and create catalog file
 signtool sign /f USBIP_TestCert.pfx /t http://timestamp.verisign.com/scripts/timestamp.dll output\USBIPEnum_x86.sys
 signtool sign /f USBIP_TestCert.pfx /t http://timestamp.verisign.com/scripts/timestamp.dll output\USBIPEnum_x64.sys
 
-inf2cat /driver:output /os:XP_x86,XP_x64,Server2003_X86,Server2003_X64,Vista_X86,Vista_X64,Server2008_X86,Server2008_X64,7_X86,7_X64,Server2008R2_X64
+inf2cat /driver:output /os:XP_x86,XP_x64,Server2003_X86,Server2003_X64,Vista_X86,Vista_X64,Server2008_X86,Server2008_X64,7_X86,7_X64,Server2008R2_X64,8_X64,8_X86
 
 signtool sign /f USBIP_TestCert.pfx /t http://timestamp.verisign.com/scripts/timestamp.dll output\USBIPEnum.cat
 
