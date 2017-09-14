c48f727f127368813baaa02ece7b298e0005d9ef
George Hopkins <george-hopkins@null.net>
2017-09-14 14:11:15 +0200

    userspace: Update solution file
    When the userspace utility is built with MSVC, a preconfigured
    config.h will be used. This removes the requirement of having
    a working autotools installation on Windows.

---
+++ userspace/config_windows.h
@@ -0,0 +1,32 @@
+/* Preconfigured version of config.h for Windows */
+#ifndef _USBIP_CONFIG_WINDOWS_H
+#define _USBIP_CONFIG_WINDOWS_H
+
+/* Name of package */
+#define PACKAGE "usbip"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "usbip-devel@lists.sourceforge.net"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "usbip"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "usbip 0.2.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "usbip"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.2.0"
+
+/* numeric version number */
+#define USBIP_VERSION 0x000106
+
+/* Version number of package */
+#define VERSION "0.2.0"
+
+#endif
