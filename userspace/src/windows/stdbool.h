be586c7eaa197cf9c7607a49ba8f3f1a73fa801a
George Hopkins <george-hopkins@null.net>
2017-09-14 20:27:51 +0200

    userspace: Add stdbool.h for MSVC

---
+++ userspace/src/windows/stdbool.h
@@ -0,0 +1,14 @@
+#ifndef _STDBOOL
+#define _STDBOOL
+
+#ifndef _UCRT
+typedef int _Bool;
+#endif
+
+#define bool _Bool
+#define true 1
+#define false 0
+
+#define __bool_true_false_are_defined 1
+
+#endif /* _STDBOOL */
